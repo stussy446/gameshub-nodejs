4e2477c2fc466dbb934813e59c4038ac61c0dbb1
Steve Rector <steverector27@gmail.com>
2024-03-13 11:25:05 -0700

    Add mongoose and config to connect to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 dotenv.config();
 
 const { PORT } = process.env;
+
+let { DATABASE } = process.env;
+DATABASE = DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DATABASE).then(() => {
+  console.log('DB Connection successful');
+});
 
 // Has application listen on specified port
 app.listen(PORT, () => {
